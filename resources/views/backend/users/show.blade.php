0b35c178731fdf7863eda6f4a5cc448949a1dcc7
Mohammad Saifur Rahman <saifur.rahman@sslwireless.com>
2021-12-12 13:38:37 +0600

    update

--- resources/views/backend/users/show.blade.php
+++ resources/views/backend/users/show.blade.php
@@ -7,23 +7,26 @@
 @section('content')
 
     <div class="container-fluid">
-        <!-- Page-Title -->
-        {{-- <div class="row">
-            <div class="col-sm-12">
-                <div class="page-title-box">
-                    <div class="float-right">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript:void(0);">Marks Quiz Carnival</a></li>
-                            <li class="breadcrumb-item active">Users</li>
+
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0 font-size-18">User Data</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}">Users</a></li>
+                            <li class="breadcrumb-item active">User Data</li>
                         </ol>
                     </div>
-                    <h4 class="page-title">User Details</h4></div>
-                <!--end page-title-box-->
+
+                </div>
             </div>
-            <!--end col-->
-        </div> --}}
-        <!--end row-->
-        <!-- end page title end breadcrumb -->
+        </div>
+        <!-- end page title -->
+
         <div class="row">
             <div class="col-12">
                 <div class="card">
